6ba0d12749d1f9f5b5d125b9552c2db6b38bfb21
Gabriel Cabrejas <gabriel@cabrejas.net>
2025-06-13 12:32:44 -0700

    wip

--- api/pet-model-prisma.js
+++ api/pet-model-prisma.js
@@ -2,29 +2,34 @@ const prisma = require('../prisma/prisma')
 
 module.exports = {
   async find(where) {
-    // [GET] http://localhost:5432/api/pets?type=dog
+    // GET http://localhost:5432/api/pets?type=dog
     // SELECT * FROM "Pet" WHERE type='dog';
     const pets = await prisma.Pet.findMany({ where })
     return pets
   },
 
   async findById(id) {
-    // [GET] http://localhost:5432/api/pets/1
-    // SELECT * FROM "Pet" WHERE id = 1
+    // GET http://localhost:5432/api/pets/1
+    // SELECT * FROM "Pet" WHERE id = 1;
     const pets = await prisma.Pet.findMany({ where })
     return pets
   },
 
   async create({ name, type, age }) {
-    // [POST] http://localhost:5432/api/pets/1 { name: "Fido", type: "dog": age: 5 }
-    // INSERT INTO "Pet" (name, type, age) VALUES (...)
+    // POST http://localhost:5432/api/pets/1 { name: "Fido", type: "dog": age: 5 }
+    // INSERT INTO "Pet" (name, type, age) VALUES ('Fido', 'dog', 5);
     const created = await prisma.Pet.create({ data: { name, type, age } })
     return created
   },
 
   async update(id, changes) {
-    // UPDATE "Pet" SET ... WHERE id = 1
-
+    // PUT http://localhost:5432/api/pets/1 { adopted: true }
+    // UPDATE "Pet" SET adopted = true WHERE id = 1;
+    const updated = await prisma.Pet.update({
+      where: { id: Number(id) },
+      data: changes,
+    })
+    return updated
   },
 
   async delete(id) {
