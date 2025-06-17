e0dbafdf0106cd00fa25427451c6fe6cf8cd7162
Gabriel Cabrejas <gabriel@cabrejas.net>
2025-06-17 11:05:05 -0700

    wip

--- api/pet-model-prisma.js
+++ api/pet-model-prisma.js
@@ -1,32 +1,37 @@
 // import prisma client lib and instantiate
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
 
 module.exports = {
   async find(where) {
-    // GET http://localhost:5432/api/pets?type=dog
+    // GET http://localhost:3000/api/pets?type=dog
     // SELECT * FROM "Pet" WHERE type='dog';
-
+    const pets = await prisma.pet.findMany({ where: where })
+    return pets
   },
 
   async findById(id) {
-    // GET http://localhost:5432/api/pets/1
+    // GET http://localhost:3000/api/pets/1
     // SELECT * FROM "Pet" WHERE id = 1;
-
+    const pet = await prisma.pet.findUnique({ where: { id } })
+    return pet
   },
 
   async create(changes) {
-    // POST http://localhost:5432/api/pets/1 { name: "Fido", type: "dog": age: 5 }
+    // POST http://localhost:3000/api/pets/1 { name: "Fido", type: "dog": age: 5 }
     // INSERT INTO "Pet" (name, type, age) VALUES ('Fido', 'dog', 5);
 
   },
 
   async update(id, changes) {
-    // PUT http://localhost:5432/api/pets/1 { adopted: true }
+    // PUT http://localhost:3000/api/pets/1 { adopted: true }
     // UPDATE "Pet" SET adopted = true WHERE id = 1;
 
   },
 
   async delete(id) {
-    // DELETE http://localhost:5432/api/pets/1
+    // DELETE http://localhost:3000/api/pets/1
     // DELETE FROM "Pet" WHERE id = 1;
 
   },
