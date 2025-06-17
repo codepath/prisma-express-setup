3af1449f5aec93a5fc017f4dbfaf223496cdbf8a
Gabriel Cabrejas <gabriel@cabrejas.net>
2025-06-17 10:42:44 -0700

    generate migration

---
+++ prisma/migrations/20250617174059_create_pet_table/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Pet" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "age" INTEGER,
+    "adopted" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Pet_pkey" PRIMARY KEY ("id")
+);
